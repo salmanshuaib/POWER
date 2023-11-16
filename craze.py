277df14aa96c81b49e0948845c150293d86c679a
Salman Shuaib <salman.shuaib@gmail.com>
2023-11-15 21:27:00 -0500

    Ty C:"

---
+++ craze.py
@@ -0,0 +1,69 @@
+import csv
+import datetime
+import time
+import winsound  # For Windows systems, for beeping
+
+# Function to parse the time format in Scheduler.csv and convert it to a datetime object
+def parse_time(time_str):
+    try:
+        time_format = "%H%M hours"
+        if "to" in time_str:
+            time_parts = time_str.split(" to ")
+            start_time = datetime.datetime.strptime(time_parts[0], time_format)
+            end_time = datetime.datetime.strptime(time_parts[1], time_format)
+            return start_time, end_time
+        else:
+            return datetime.datetime.strptime(time_str, time_format)
+    except ValueError:
+        raise ValueError(f"Invalid time format: {time_str}")
+
+# Function to check if the current time matches the scheduled time
+def is_time_to_execute(scheduled_time):
+    current_time = datetime.datetime.now().time()
+    if isinstance(scheduled_time, tuple):
+        return scheduled_time[0].time() <= current_time <= scheduled_time[1].time()
+    else:
+        return scheduled_time.time() == current_time
+
+# Function to read actions from the 'actions' file
+def read_actions():
+    with open('actions.txt', 'r') as actions_file:
+        actions = actions_file.read().splitlines()
+    return actions
+
+# Function to count the number of YES answers
+def count_yes_answers():
+    yes_count = 0
+    for i, task in enumerate(actions):
+        question = f"Did you complete TASK{i + 1}: 1/0?"
+        user_input = input(question)
+        if user_input == "1":
+            yes_count += 1
+    return yes_count
+
+# Read actions from the 'actions' file
+actions = read_actions()
+
+# Read scheduled tasks from 'Scheduler.csv' and store them in a list of tuples
+scheduled_tasks = []
+with open('Scheduler.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        task = row['TASK']
+        time_range = row['TIME']
+        time_range = parse_time(time_range)
+        scheduled_tasks.append((task, time_range))
+
+# Check if it's time to execute each task
+for task, time_range in scheduled_tasks:
+    while not is_time_to_execute(time_range):
+        time.sleep(60)  # Sleep for a minute before checking again
+
+    # Beep to notify the user
+    winsound.Beep(1000, 1000)  # Beep for 1 second
+
+    # Ask the question and count YES answers
+    yes_count = count_yes_answers()
+    print(f"Task {task}: You answered YES {yes_count} times out of {len(actions)}")
+
+print("All tasks completed!")
