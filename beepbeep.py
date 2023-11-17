7b7250688c6404f877820478f9fbb481eac0e302
Salman Shuaib <salman.shuaib@gmail.com>
2023-11-17 16:39:27 -0500

    actions -> tasks

---
+++ beepbeep.py
@@ -0,0 +1,8 @@
+import winsound
+
+# Frequency and duration for the beep (you can adjust these values)
+frequency = 1000  # Frequency in Hertz (e.g., 1000 Hz)
+duration = 1000  # Duration in milliseconds (e.g., 1000 ms or 1 second)
+
+# Produce a beep sound
+winsound.Beep(frequency, duration)
--- craze.py
+++ craze.py
@@ -27,45 +27,45 @@ def is_time_to_execute(scheduled_time):
     else:
         return scheduled_time.time() == current_time
 
-# Function to read actions from the 'actions' file
-def read_actions():
-    with open('actions', 'r') as action_file:
-        action = action_file.read().splitlines()
-    return action
+# Function to read tasks from the 'tasks' file
+def read_tasks():
+    with open('TaskList', 'r') as tasks_file:
+        tasks = tasks_file.read().splitlines()
+    return tasks
 
 # Function to count the number of YES answers
 def count_yes_answers():
     yes_count = 0
-    for i, task in enumerate(action):
+    for i, task in enumerate(tasks):
         question = f"Did you complete TASK{i + 1}: 1/0?"
         user_input = input(question)
         if user_input == "1":
             yes_count += 1
     return yes_count
 
-# Read actions from the 'actions' file
-action = read_actions()
+# Read tasks from the 'tasks' file
+tasks = read_tasks()
 
 # Read scheduled tasks from 'Scheduler.csv' and store them in a list of tuples
 scheduled_tasks = []
 with open('Scheduler.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        task = row['TASK']
-        time_range = row['TIME']
+        task = row['Task']
+        time_range = row['Time']
         time_range = parse_time(time_range)
         scheduled_tasks.append((task, time_range))
 
 # Check if it's time to execute each task
 for task, time_range in scheduled_tasks:
     while not is_time_to_execute(time_range):
-        time.sleep(25)  # Sleep for 25 seconds before checking again
+        time.sleep(22)  # Sleep for 22 seconds before checking again
 
     # Beep to notify the user
     winsound.Beep(1000, 1000)  # Beep for 1 second
 
     # Ask the question and count YES answers
     yes_count = count_yes_answers()
-    print(f"Task {task}: You answered YES {yes_count} times out of {len(action)}")
+    print(f"Task {task}: You answered YES {yes_count} times out of {len(tasks)}")
 
 print("All tasks completed!")
